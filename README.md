5dd3cb59cabda0ba32bed4294197682e6ef54404
Mathew Topper <damm_horse@yahoo.co.uk>
2018-09-18 14:48:12 +0100

    Initial import

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # python-magic-win64
+
+Windows 64-bit spin of python-magic for conda
+
+Source code retrieved from https://github.com/julian-r/python-magic
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='python-magic',
+      description='File type identification using libmagic binary package',
+      author='Julian David Rath',
+      author_email='julian.rath@gmail.com',
+      url="http://github.com/julian-r/python-magic",
+      version='0.4.14',
+      package_data={
+            'magic': ['libmagic/*.dll', 'libmagic/*.mgc'],
+      },
+      zip_safe=False,
+      packages=['magic'],
+      long_description="""This module uses ctypes to access the libmagic file type
+identification library.  It makes use of the local magic database and
+supports both textual and MIME-type output.
+""",
+      keywords="mime magic file binary",
+      license="MIT",
+      test_suite='test',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Operating System :: Microsoft :: Windows'
+      ],
+)
